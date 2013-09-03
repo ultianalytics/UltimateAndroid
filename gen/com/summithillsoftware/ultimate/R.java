5cdc7dca427b42a88f272fc3396b8a045b45e84b
Jim Geppert <jimgeppert@gmail.com>
2013-09-03 10:01:24 -0500

    added Team activity and fragment (no hooked up yet)

--- gen/com/summithillsoftware/ultimate/R.java
+++ gen/com/summithillsoftware/ultimate/R.java
@@ -243,28 +243,49 @@ public final class R {
         public static final int team=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int icon_team=0x7f090002;
+        public static final int action_settings=0x7f09000f;
+        public static final int icon_team=0x7f09000d;
+        public static final int label_team_playerdisplay=0x7f090008;
+        public static final int label_team_type=0x7f090003;
+        public static final int radio_team_playerdisplay_name=0x7f09000a;
+        public static final int radio_team_playerdisplay_number=0x7f09000b;
+        public static final int radio_team_type_mixed=0x7f090006;
+        public static final int radio_team_type_uni=0x7f090005;
+        public static final int radiogroup_team_playerdisplay=0x7f090009;
+        public static final int radiogroup_team_type=0x7f090004;
+        public static final int tableRow1=0x7f090002;
+        public static final int tableRow2=0x7f090007;
         public static final int teamsFragment=0x7f090000;
-        public static final int teamsListView=0x7f090001;
-        public static final int text_team_cloud_id=0x7f090004;
-        public static final int text_team_name=0x7f090003;
+        public static final int teamsListView=0x7f09000c;
+        public static final int text_team_cloud_id=0x7f09000e;
+        public static final int text_team_name=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_teams=0x7f030001;
-        public static final int fragment_teams=0x7f030002;
-        public static final int rowlayout_teams=0x7f030003;
+        public static final int activity_team=0x7f030001;
+        public static final int activity_teams=0x7f030002;
+        public static final int fragment_team=0x7f030003;
+        public static final int fragment_teams=0x7f030004;
+        public static final int rowlayout_teams=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f080000;
-        public static final int teams=0x7f080001;
+        public static final int team=0x7f080001;
+        public static final int teams=0x7f080002;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060003;
         public static final int image_description=0x7f060001;
+        public static final int label_team_playerdisplay=0x7f06000a;
+        public static final int label_team_type=0x7f060007;
+        public static final int label_team_type_mixed=0x7f060009;
+        public static final int label_team_type_uni=0x7f060008;
+        public static final int placeholder_team_name=0x7f060006;
+        public static final int radio_team_playerdisplay_name=0x7f06000b;
+        public static final int radio_team_playerdisplay_number=0x7f06000c;
+        public static final int title_activity_team=0x7f060005;
         public static final int title_activity_teams=0x7f060004;
     }
     public static final class style {
