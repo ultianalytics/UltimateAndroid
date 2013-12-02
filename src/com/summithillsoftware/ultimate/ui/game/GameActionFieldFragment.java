cace0b5bc8519db159568c75f17e3e293a96c404
Jim Geppert <jimgeppert@gmail.com>
2013-12-02 08:36:55 -0600

    added fragments for action activity

--- src/com/summithillsoftware/ultimate/ui/game/GameActionFieldFragment.java
+++ src/com/summithillsoftware/ultimate/ui/game/GameActionFieldFragment.java
@@ -8,11 +8,11 @@ import android.view.ViewGroup;
 import com.summithillsoftware.ultimate.R;
 import com.summithillsoftware.ultimate.ui.UltimateFragment;
 
-public class GameActionFragment extends UltimateFragment {
+public class GameActionFieldFragment extends UltimateFragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_game_action, container);
+		View view = inflater.inflate(R.layout.fragment_game_action_field, container);
 		return view;
 	}
 
