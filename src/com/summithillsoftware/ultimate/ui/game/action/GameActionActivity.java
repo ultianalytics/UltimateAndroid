7be3e51f5b240dc6340ffd925442b30074dd536a
Jim Geppert <jimgeppert@gmail.com>
2013-12-30 15:05:54 -0600

    removed dead import

--- src/com/summithillsoftware/ultimate/ui/game/action/GameActionActivity.java
+++ src/com/summithillsoftware/ultimate/ui/game/action/GameActionActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 
 import com.summithillsoftware.ultimate.R;
 import com.summithillsoftware.ultimate.model.Action;
