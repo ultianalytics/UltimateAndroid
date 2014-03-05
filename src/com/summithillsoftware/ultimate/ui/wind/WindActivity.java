75ad7e04967ea4747f8bbf8c92a1c9379ae73c5c
Jim Geppert <jimgeppert@gmail.com>
2014-03-05 07:04:26 -0600

    Wind WIP

--- src/com/summithillsoftware/ultimate/ui/wind/WindActivity.java
+++ src/com/summithillsoftware/ultimate/ui/wind/WindActivity.java
@@ -3,16 +3,25 @@ package com.summithillsoftware.ultimate.ui.wind;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 import com.summithillsoftware.ultimate.R;
+import com.summithillsoftware.ultimate.model.Wind;
 import com.summithillsoftware.ultimate.ui.UltimateActivity;
 
 public class WindActivity extends UltimateActivity {
+	private ImageButton buttonDirectionRight;
+	private ImageButton buttonDirectionLeft;
+	private SeekBar windSpeedSeekBar;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_wind);
+		connectWidgets();
+		populateView();
 	}
 
 	@Override
@@ -25,5 +34,24 @@ public class WindActivity extends UltimateActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		return super.onOptionsItemSelected(item);
 	}
+	
+	private void connectWidgets() {
+		buttonDirectionRight = (ImageButton)findViewById(R.id.windFragment).findViewById(R.id.buttonDirectionRight);
+		buttonDirectionLeft = (ImageButton)findViewById(R.id.windFragment).findViewById(R.id.buttonDirectionLeft);
+		windSpeedSeekBar = (SeekBar)findViewById(R.id.windFragment).findViewById(R.id.windSpeedSeekBar);
+	}
+	
+	private void populateView() {
+		updateFirstPullDirectionArrow();
+	}
 
+	private void updateFirstPullDirectionArrow() {
+		boolean isRight = getWind() == null || getWind().isFirstPullLeftToRight();
+		buttonDirectionRight.setSelected(isRight);
+		buttonDirectionLeft.setSelected(!isRight);
+	}
+	
+	private Wind getWind() {
+		return null;
+	}
 }
