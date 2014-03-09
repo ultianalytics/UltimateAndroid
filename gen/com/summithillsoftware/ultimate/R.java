91e8d6f316b2ee5479bf7039ab31615e9d93eeb9
Jim Geppert <jimgeppert@gmail.com>
2014-03-08 20:15:59 -0600

    WIP on updating game start date/time

--- gen/com/summithillsoftware/ultimate/R.java
+++ gen/com/summithillsoftware/ultimate/R.java
@@ -1187,18 +1187,19 @@ containing a value of this type.
         public static final int activity_horizontal_margin=0x7f090011;
         public static final int activity_vertical_margin=0x7f090012;
         public static final int cloud_dialog_content_view_length=0x7f090023;
+        public static final int date_time_picker_height=0x7f09002a;
         public static final int event_player_check_icon_dimension=0x7f090022;
         public static final int event_player_list_font_size=0x7f090020;
         public static final int event_player_list_show_all_font_size=0x7f090021;
         public static final int event_player_list_width=0x7f09001f;
-        public static final int font_size10=0x7f09002a;
-        public static final int font_size20=0x7f09002b;
-        public static final int font_size30=0x7f09002c;
-        public static final int font_size40=0x7f09002d;
-        public static final int font_size50=0x7f09002e;
-        public static final int font_size60=0x7f09002f;
-        public static final int font_size70=0x7f090030;
-        public static final int font_size80=0x7f090031;
+        public static final int font_size10=0x7f09002b;
+        public static final int font_size20=0x7f09002c;
+        public static final int font_size30=0x7f09002d;
+        public static final int font_size40=0x7f09002e;
+        public static final int font_size50=0x7f09002f;
+        public static final int font_size60=0x7f090030;
+        public static final int font_size70=0x7f090031;
+        public static final int font_size80=0x7f090032;
         public static final int form_text_size=0x7f090014;
         /**  LANDSCAPE
          Customize dimensions originally defined in res/values/dimens.xml (such as
